4e241fd2d0bdbf20e580a247e960ebe61fc557f3
Edgars Košovojs <kosovojs@gmail.com>
2021-11-18 12:22:41 +0200

    add install_requires and bumb version

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ with open('README.md', 'r') as fh:
 setuptools.setup(
 	name = 'geojsonformat',
 	packages = ['geojsonformat'],
-	version = '0.0.2',
+	version = '0.0.3',
 	license='MIT',
 	description = 'format geojson',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
+	install_requires=['click'],
 	author = 'Edgars Košovojs',
 	author_email = 'kosovojs@gmail.com',
 	url = 'https://github.com/kosovojs/geojsonformat',
